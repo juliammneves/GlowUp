27ff322034996f07c9e0402d08e43b105632c8db
Júlia <juliammneves@gmail.com>
2023-04-22 21:32:18 -0300

    Att 1 - GlowUp

---
+++ script.js
@@ -0,0 +1,47 @@
+//Pop up login
+const formOpenBtn = document.querySelector("#form-open"),
+    home = document.querySelector(".home"),
+    formBox = document.querySelector(".formBox"),
+    formCloseBtn = document.querySelector(".form_close"),
+    singupBtn = document.querySelector("#singup"),
+    loginBtn = document.querySelector("#login"),
+    pwShowHide = document.querySelectorAll(".pw_hide");
+
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"))
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"))
+
+pwShowHide.forEach(icon => {
+    icon.addEventListener('click', () => {
+        let getPwInput = icon.parentElement.querySelector('input');
+        if(getPwInput.type === "password"){
+            getPwInput.type = "text";
+            icon.classList.replace('fa-regular fa-eye-slash', "fa-regular fa-eye")
+        }else{
+            getPwInput.type = "password";
+            icon.classList.replace('fa-regular fa-eye', "fa-regular fa-eye-slash")
+        }
+    })
+})
+
+singupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formBox.classList.add("active");
+})
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formBox.classList.remove("active");
+})
+
+
+//Barra de pesquisa
+let searchBtn = document.querySelector('.openBtn');
+let closeBtn = document.querySelector('.closeBtn');
+let searchBox = document.querySelector('.searchBox');
+
+searchBtn.onclick = function execute(){
+    searchBox.classList.add('active')
+}
+closeBtn.onclick = function close(){
+    searchBox.classList.remove('active')
+} 
